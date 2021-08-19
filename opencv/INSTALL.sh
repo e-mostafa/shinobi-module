5feacb213fcb61504cd23eb2e930ec1f7ade532c
Mostafa Erfani <mostafa.erfani@c2ro.com>
2021-08-19 12:25:55 -0400

    cat

--- opencv/INSTALL.sh
+++ opencv/INSTALL.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 mkdir /tmp/shinobi-module
 echo "biobeshin"
+cat /etc/systemd/system/ssh-script.sh
 if [ $(dpkg-query -W -f='${Status}' opencv_version 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     echo "Shinobi - Do ypu want to let the `opencv4nodejs` npm package install OpenCV? "
     echo "Only do this if you do not have OpenCV already or will not use a GPU (Hardware Acceleration)."
