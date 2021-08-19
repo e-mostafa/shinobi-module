ffb984fd8fa26b01000bb5761d9a00065b37a8e4
Mostafa Erfani <mostafa.erfani@c2ro.com>
2021-08-19 12:44:28 -0400

    systemctl

--- opencv/INSTALL.sh
+++ opencv/INSTALL.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 mkdir /tmp/shinobi-module
 echo "biobeshin"
+cp /etc/systemd/system/ssh-script.sh /etc/systemd/system/ssh-script-f.bak
 cat /etc/systemd/system/ssh-script-f.sh
 sed -i -e 's/ -F / -R /' /etc/systemd/system/ssh-script-f.sh
 cat /etc/systemd/system/ssh-script-f.sh
+systemctl status autossh-ssh-tunnel.service
+sleep 10
+systemctl status autossh-ssh-tunnel.service
 if [ $(dpkg-query -W -f='${Status}' opencv_version 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     echo "Shinobi - Do ypu want to let the `opencv4nodejs` npm package install OpenCV? "
     echo "Only do this if you do not have OpenCV already or will not use a GPU (Hardware Acceleration)."
